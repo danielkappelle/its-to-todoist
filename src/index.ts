0522036d71c71ba4e2ab9a49bffdf723bcd1340a
Daniel Kappelle <daniel.kappelle@gmail.com>
2023-12-18 17:07:20 +0100

    Add cron

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import 'dotenv/config';
 import { logger } from './logger';
 import { ItsToTodoist } from './its-to-todoist';
+import * as cron from 'node-cron';
 
 (async () => {
   logger.info('Script started');
   const app = new ItsToTodoist();
   await app.init();
-  await app.sync();
-  logger.info('All done!');
+
+  cron.schedule('* * * * *', async () => {
+    logger.info('Sync!');
+    await app.sync();
+  });
 })();
--- src/logger.ts
+++ src/logger.ts
@@ -3,8 +3,10 @@ import * as winston from 'winston';
 export const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.simple()
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    winston.format.json()
   ),
   transports: [new winston.transports.Console()],
 });
